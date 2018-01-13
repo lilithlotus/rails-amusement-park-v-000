75add9acf33e82f08471dd4e47fb52a97294c299
Jessica Bensch <jessica.bensch11@gmail.com>
2018-01-13 08:30:38 -0500

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -3,50 +3,26 @@ class Ride < ActiveRecord::Base
   belongs_to :user
   belongs_to :attraction
 
+  def take_ride
 
+    if self.user.tickets < self.attraction.tickets && self.user.height < self.attraction.min_height
+      "Sorry. You do not have enough tickets to ride the #{self.attraction.name}. You are not tall enough to ride the #{self.attraction.name}."
+    elsif self.user.tickets < self.attraction.tickets
+     "Sorry. You do not have enough tickets to ride the #{self.attraction.name}."
+    elsif self.user.height < self.attraction.min_height
+     "Sorry. You are not tall enough to ride the #{self.attraction.name}."
+    else
+      #binding.pry
+      @user = User.find_by(id: session[:user_id])
+      new_tickets = self.user.tickets - self.attraction.tickets
+      new_happiness = self.user.happiness + self.attraction.happiness_rating
+      new_nausea = self.user.nausea + self.attraction.nausea_rating
+      @user.update(tickets: new_tickets, happiness: new_happiness, nausea: new_nausea)
+      @user.save
 
-    def take_ride
-  enough_tickets, tall_enough = meet_requirements
-  if enough_tickets && tall_enough
-    start_ride
-  elsif tall_enough && !enough_tickets
-    "Sorry. " + ticket_issue
-  elsif enough_tickets && !tall_enough
-    "Sorry. " + height_issue
-  else
-    "Sorry. " + ticket_issue + " " + height_issue
+     "Thanks for riding the #{self.attraction.name}!"
+   end
+    #binding.pry
   end
-end
-
-def meet_requirements
-  enough_tickets, tall_enough = false
-  if self.user.tickets >= self.attraction.tickets
-    enough_tickets = true
-  end
-  if self.user.height >= self.attraction.min_height
-    tall_enough = true
-  end
-  return [enough_tickets, tall_enough]
-end
-
-def start_ride
-  new_happiness = self.user.happiness + self.attraction.happiness_rating
-  new_nausea = self.user.nausea + self.attraction.nausea_rating
-  new_tickets =  self.user.tickets - self.attraction.tickets
-  self.user.update(
-    :happiness => new_happiness,
-    :nausea => new_nausea,
-    :tickets => new_tickets
-  )
-  "Thanks for riding the #{self.attraction.name}!"
-end
-
-def ticket_issue
-  "You do not have enough tickets the #{self.attraction.name}."
-end
-
-def height_issue
-  "You are not tall enough to ride the #{self.attraction.name}."
-end
 
 end
