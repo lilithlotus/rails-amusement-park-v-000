0ed29c79ebdccdecd26b896720b379cdb6151602
Jessica Bensch <jessica.bensch11@gmail.com>
2018-01-13 06:34:06 -0500

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -4,22 +4,23 @@ class Ride < ActiveRecord::Base
   belongs_to :attraction
 
   def take_ride
-    @user = User.find_by(id: user_id)
-    @attraction = Attraction.find_by(id: attraction_id)
-    if @user.tickets >= @attraction.tickets && @user.height >= @attraction.min_height
-      new_tickets = @user.tickets - @attraction.tickets
-      new_happiness = @user.happiness + @attraction.happiness_rating
-      new_nausea = @user.nausea + @attraction.nausea_rating
-      @user.update(happiness: new_happiness, nausea: new_nausea, tickets: new_tickets)
-      @user.save
-    elsif @user.tickets < @attraction.tickets && @user.height < @attraction.min_height
+
+    if self.user.tickets < self.attraction.tickets && self.user.height < self.attraction.min_height
       return "Sorry. You do not have enough tickets to ride the Roller Coaster. You are not tall enough to ride the Roller Coaster."
-    elsif @user.tickets < @attraction.tickets
+    elsif self.user.tickets < self.attraction.tickets
       return "Sorry. You do not have enough tickets to ride the Roller Coaster."
-    elsif @user.height < @attraction.min_height
+    elsif self.user.height < self.attraction.min_height
       return "Sorry. You are not tall enough to ride the Roller Coaster."
+    else
+      new_tickets = self.user.tickets - self.attraction.tickets
+      new_happiness = self.user.happiness + self.attraction.happiness_rating
+      new_nausea = self.user.nausea + self.attraction.nausea_rating
+      self.user.happiness = new_happiness
+      self.user.nausea = new_nausea
+      self.user.tickets = new_tickets
+
     end
-    binding.pry
+    self.user.save
   end
 
 end
